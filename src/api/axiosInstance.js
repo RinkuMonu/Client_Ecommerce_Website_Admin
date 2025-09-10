abab29dc0853403642a5fd6324e330efa8cd7ec0
veer <veer123@gmail.com>
2025-09-10 12:54:33 +0530

    changes

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,14 +3,13 @@ import { HandleAxiosError } from "./axioserror";
 
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.jajamblockprints.com", // Replace with your actual base URL
-  // baseURL: "http://localhost:5007", // Replace with your actual base URL
+  // baseURL: "https://api.jajamblockprints.com", // Replace with your actual base URL
+  baseURL: "http://localhost:5007", // Replace with your actual base URL
   // baseURL: "https://tgzv7r4s-5008.inc1.devtunnels.ms/",
   // headers: {
   //   "Content-Type": "application/json",
   //    },
 });
-//backend kha h
 // Request Interceptor
 axiosInstance.interceptors.request.use(function (config) {
   const token = sessionStorage.getItem("accessToken");
